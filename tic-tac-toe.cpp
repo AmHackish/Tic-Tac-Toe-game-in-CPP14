dedebcf838a9a6286bd1bf8edb14625d1bb6b769
Kashish Garg <kashigarg1111@gmail.com>
2020-09-17 15:55:57 +0530

    Update tic-tac-toe.cpp

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-char aayu[3][3];
+char kash[3][3];
 
 struct move
 {
@@ -11,48 +11,48 @@ struct move
 char p='X';
 char o='O';
 
-int pendmove(char aayu[3][3])
+int pendmove(char kash[3][3])
 {
 	for (int i = 0; i < 3; i++)
 	{
 		for (int j = 0; j < 3; j++)
 		{
-			if(aayu[i][j]=='_')
+			if(kash[i][j]=='_')
 				return 1;
 		}
 	}
 	return 0;
 }
 
-int eval(char aayu[3][3])
+int eval(char kash[3][3])
 {
 	for (int i = 0; i < 3; i++)
 	{
-		if(aayu[i][0]==aayu[i][1] and aayu[i][1]==aayu[i][2] and aayu[i][1]=='X')
+		if(kash[i][0]==kash[i][1] and kash[i][1]==kash[i][2] and kash[i][1]=='X')
 			return 10;
-		if(aayu[i][0]==aayu[i][1] and aayu[i][1]==aayu[i][2] and aayu[i][1]=='O')
+		if(kash[i][0]==kash[i][1] and kash[i][1]==kash[i][2] and kash[i][1]=='O')
 			return -10;
 	}
 	for (int i = 0; i < 3; i++)
 	{
-		if(aayu[0][i]==aayu[1][i] and aayu[1][i]==aayu[2][i] and aayu[2][i]=='X')
+		if(kash[0][i]==kash[1][i] and kash[1][i]==kash[2][i] and kash[2][i]=='X')
 			return 10;
-		if(aayu[0][i]==aayu[1][i] and aayu[1][i]==aayu[2][i] and aayu[2][i]=='O')
+		if(kash[0][i]==kash[1][i] and kash[1][i]==kash[2][i] and kash[2][i]=='O')
 			return -10;
 	}
-	if(aayu[0][0]==aayu[1][1] and aayu[1][1]==aayu[2][2] and aayu[2][2]=='X')
+	if(kash[0][0]==kash[1][1] and kash[1][1]==kash[2][2] and kash[2][2]=='X')
 		return 10;
-	if(aayu[0][0]==aayu[1][1] and aayu[1][1]==aayu[2][2] and aayu[2][2]=='O')
+	if(aayu[0][0]==kash[1][1] and kash[1][1]==kash[2][2] and kash[2][2]=='O')
 		return -10;
-	if(aayu[2][0]==aayu[1][1] and aayu[1][1]==aayu[0][2] and aayu[2][0]=='X')
+	if(aayu[2][0]==kash[1][1] and kash[1][1]==kash[0][2] and kash[2][0]=='X')
 		return 10;
-	if(aayu[2][0]==aayu[1][1] and aayu[1][1]==aayu[0][2] and aayu[2][0]=='O')
+	if(aayu[2][0]==kash[1][1] and kash[1][1]==kash[0][2] and kash[2][0]=='O')
 		return -10;
 
 	return 0;
 }
 
-int minmax(char aayu[3][3], int d, bool im)
+int minmax(char kash3][3], int d, bool im)
 {
 	int score = eval(aayu);
 	if(score == 10)
@@ -60,7 +60,7 @@ int minmax(char aayu[3][3], int d, bool im)
 	if(score == -10)
 		return -10;
 
-	if(pendmove(aayu)==0)
+	if(pendmove(kash)==0)
 		return 0;
 
 	if(im)
@@ -70,11 +70,11 @@ int minmax(char aayu[3][3], int d, bool im)
 		{
 			for (int j = 0; j < 3; j++)
 			{
-				if(aayu[i][j]=='_')
+				if(kash[i][j]=='_')
 				{
-					aayu[i][j]=p;
-					b=max(b,minmax(aayu,d+1, !im));
-					aayu[i][j]='_';
+					kash[i][j]=p;
+					b=max(b,minmax(kash,d+1, !im));
+					kash[i][j]='_';
 				}				
 			}
 		}
@@ -87,11 +87,11 @@ int minmax(char aayu[3][3], int d, bool im)
 		{
 			for (int j = 0; j < 3; j++)
 			{
-				if(aayu[i][j]=='_')
+				if(kash[i][j]=='_')
 				{
-					aayu[i][j]=o;
-					b=min(b,minmax(aayu,d+1, !im));
-					aayu[i][j]='_';
+					kash[i][j]=o;
+					b=min(b,minmax(kash,d+1, !im));
+					kash[i][j]='_';
 				}				
 			}
 		}
@@ -99,7 +99,7 @@ int minmax(char aayu[3][3], int d, bool im)
 	}
 }
 
-move bestmove(char aayu[3][3])
+move bestmove(char kash[3][3])
 {
 	int bestval= -100000000;
 	move bestMove;
@@ -109,11 +109,11 @@ move bestmove(char aayu[3][3])
 	{
 		for (int j = 0; j < 3; j++)
 		{
-			if(aayu[i][j]=='_')
+			if(kash[i][j]=='_')
 			{
-				aayu[i][j] = p;
-				int tmp = minmax(aayu , 0 , false);
-				aayu[i][j]='_';
+				kash[i][j] = p;
+				int tmp = minmax(kash , 0 , false);
+				kash[i][j]='_';
 				if(tmp>bestval)
 				{
 					bestMove.r=i;
@@ -130,28 +130,32 @@ int main()
 {
 	while(true)
 	{
-		cout << "Welcome to Tic-Tac-Toe by CodeAayu , Aayush Ahuja. Enter N for new game or any other key for exiting." << endl;
+		cout << "Welcome to Tic-Tac-Toe by AmHackis, Kashish Garg. Enter N for new game or any other key for exiting." << endl;
 		char tm;
 		cin >> tm;
 		if(tm=='N' or tm=='n')
 		{
 			for(int i=0; i<3 ; i++)
 				for(int j=0; j<3 ; j++)
-					aayu[i][j]='_';
+					kash[i][j]='_';
 			cout << "Simple rules , enter row no. and column no. respectively for your choice , you are O , i am X , so lets play, choose your first entry." << endl;
-			while(pendmove(aayu))
+			while(pendmove(kash))
 			{
 				int row , col, testing;
 				cin >> row >> col;
-				while(aayu[row-1][col-1]!='_')
+				while(kash[row-1][col-1]!='_')
 				{
 					cout << "Wrong Move. You can't overwrite me. Re-enter your move." << endl;
-					cout << aayu[0][0] << "|" << aayu[0][1] << "|" << aayu[0][2] << endl << aayu[1][0] << "|" << aayu[1][1] << "|" << aayu[1][2] << endl << aayu[2][0] << "|" << aayu[2][1] << "|" << aayu[2][2] << endl;
+					cout << kash[0][0] << "|" << kash[0][1] << "|" << kash[0][2] << endl;
+					cout << kash[1][0] << "|" << kash[1][1] << "|" << kash[1][2] << endl;
+					cout << kash[2][0] << "|" << kash[2][1] << "|" << kash[2][2] << endl;
 					cin >> row >> col;
 				}
-				aayu[row-1][col-1]=o;
-				cout << aayu[0][0] << "|" << aayu[0][1] << "|" << aayu[0][2] << endl << aayu[1][0] << "|" << aayu[1][1] << "|" << aayu[1][2] << endl << aayu[2][0] << "|" << aayu[2][1] << "|" << aayu[2][2] << endl;
-				testing = eval(aayu);
+				kash[row-1][col-1]=o;
+				cout << kash[0][0] << "|" << kash[0][1] << "|" << kash[0][2] << endl;
+				cout << kash[1][0] << "|" << kash[1][1] << "|" << kash[1][2] << endl;
+				cout << kash[2][0] << "|" << kash[2][1] << "|" << kash[2][2] << endl;
+				testing = eval(kash);
 				if(testing<0)
 				{
 					cout << "You won!" << endl;
